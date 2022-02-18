64eb7c71d0ebc3c71d00c086fbb9568cddf59483
glfr3d9 <fad-2011@mail.ru>
2022-02-18 03:25:16 +0300

    Поиск простых чисел(правки 2)

--- src/Primes.java
+++ src/Primes.java
@@ -11,7 +11,7 @@ public class Primes {
         }
     }
 
-    //Проверка
+    //Проверка на простое число
     public static boolean isPrime(int n) {
         for (int i = 2; i < n; i++) {
             if (n%i == 0){
